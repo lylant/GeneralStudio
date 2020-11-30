91ae71833ee64ad9b5242668eaf3ca50962efb33
Lylantz <pphantasm@gmail.com>
2020-11-30 22:27:53 +1100

    Add GetUserInfos and checkUserInfo

--- U10301792/NetCore.Services/Svcs/UserService.cs
+++ U10301792/NetCore.Services/Svcs/UserService.cs
@@ -1,17 +1,32 @@
-﻿using System;
+﻿using NetCore.Data.DataModels;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace NetCore.Services.Svcs
 {
     public class UserService
     {
-        /**
-         * a method to get a user information
-         */
-        public IEnumerable<User> GetUserInfos()
+        #region private methods
+        private IEnumerable<User> GetUserInfos()
         {
-            
+            return new List<User>()
+            {
+                new User()
+                {
+                    UserID = "simonlee",
+                    UserName = "Simon Lee",
+                    UserEmail = "19978707@student.westernsydney.edu.au",
+                    Password = "19978707"
+                }
+            };
         }
+
+        private bool checkUserInfo(string userID, string password)
+        {
+            return GetUserInfos().Where(u => u.UserID.Equals(userID) && u.Password.Equals(password)).Any();
+        }
+        #endregion
     }
 }

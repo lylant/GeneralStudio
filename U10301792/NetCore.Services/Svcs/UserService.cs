d405705e64456b397a929769d649ca18db384e10
Lylantz <pphantasm@gmail.com>
2020-11-30 22:18:39 +1100

    Add a project NetCore.Data

---
+++ U10301792/NetCore.Services/Svcs/UserService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetCore.Services.Svcs
+{
+    public class UserService
+    {
+        /**
+         * a method to get a user information
+         */
+        public IEnumerable<User> GetUserInfos()
+        {
+            
+        }
+    }
+}

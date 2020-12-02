30e56bc607ebe2408c88d1f696babcae756ff034
Lylantz <pphantasm@gmail.com>
2020-12-02 19:57:18 +1100

    CodeFirst practices #1

--- U10301792/NetCore.Data/DataModels/User.cs
+++ U10301792/NetCore.Data/DataModels/User.cs
@@ -1,15 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace NetCore.Data.DataModels
 {
     public class User
     {
+        [Key, StringLength(50), Column(TypeName = "varchar(50)")]
         public string UserID { get; set; }
+
+        [Required, StringLength(100), Column(TypeName = "nvarchar(100)")]
         public string UserName { get; set; }
+
+        [Required, StringLength(320), Column(TypeName = "nvarchar(320)")]
         public string UserEmail { get; set; }
+
+        [Required, StringLength(130), Column(TypeName = "nvarchar(130)")]
         public string Password { get; set; }
+
+        [Required]
+        public bool IsMembershipWithdrawn { get; set; }
+
+        [Required]
         public DateTime DateJoinedUTC { get; set; }
     }
 }
---
+++ U10301792/NetCore.Data/DataModels/UserRolesByUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace NetCore.Data.DataModels
+{
+    public class UserRolesByUser
+    {
+        [Key, StringLength(50), Column(TypeName = "varchar(50)")]
+        public string UserID { get; set; }
+
+        [Key, StringLength(50), Column(TypeName = "varchar(50)")]
+        public string RoleID { get; set; }
+
+        [Required]
+        public DateTime OwnedDateUTC { get; set; }
+    }
+}

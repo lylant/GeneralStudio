ddc72a77e8fb9b9150fd1f314753de28676e014d
Lylant <pphantasm@gmail.com>
2021-09-02 14:21:37 +1000

    initiate orchid-test project

---
+++ U4847018/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public IActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            return View();
+        }
+    }
+}
